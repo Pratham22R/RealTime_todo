efb8b5a3e854c380fd564a238a65cfe8c8f098c4
Pratham22R <pratham2262003@gmail.com>
2025-08-20 13:59:56 +0530

    UI enhanced

--- Frontend/src/components/Auth/LoginForm.jsx
+++ Frontend/src/components/Auth/LoginForm.jsx
@@ -181,7 +181,7 @@ const LoginForm = () => {
           {/* Footer */}
           <p className="text-sm text-center text-gray-500 mt-6">
             Don’t have an account?{" "}
-            <Link to="/signup" className="text-blue-600 hover:underline">
+            <Link to="/register" className="text-blue-600 hover:underline">
               Sign up
             </Link>
           </p>
